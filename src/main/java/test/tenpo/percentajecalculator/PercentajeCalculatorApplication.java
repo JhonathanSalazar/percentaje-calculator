7d3e1f964e9c31ad2aa47b3576c9facf2813dd66
Jhonathan Salazar <salajhon69@gmail.com>
2025-05-06 17:17:57 -0500

    feat: project init

---
+++ src/main/java/test/tenpo/percentajecalculator/PercentajeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package test.tenpo.percentajecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercentajeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PercentajeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/tenpo/percentajecalculator/PercentajeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package test.tenpo.percentajecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PercentajeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
