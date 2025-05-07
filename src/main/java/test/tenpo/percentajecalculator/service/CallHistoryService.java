f3aa38b1ddc7c76fe45b0c79285253f35efa1850
Jhonathan Salazar <salajhon69@gmail.com>
2025-05-07 18:43:47 -0500

    fix: remove unused import

--- src/main/java/test/tenpo/percentajecalculator/service/CallHistoryService.java
+++ src/main/java/test/tenpo/percentajecalculator/service/CallHistoryService.java
@@ -1,6 +1,5 @@
 package test.tenpo.percentajecalculator.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
